5994065ccf668a3653869a5b78b8692f8ac1a421
Mykhailo Marchuk <smoke22catches@gmail.com>
2026-03-07 10:51:02 +0000

    Add training pipeline for watermark encoder

--- model/utils.py
+++ model/utils.py
@@ -2,8 +2,8 @@ import torch
 from insightface.app import FaceAnalysis
 import numpy as np
 
-model = FaceAnalysis(name="buffalo_l")
-model.prepare(ctx_id=0, det_thresh=0.5, det_size=(640, 640))
+model = FaceAnalysis(name="buffalo_sc", providers=["CUDAExecutionProvider"])
+model.prepare(ctx_id=0, det_thresh=0.5, det_size=(320, 320))
 
 
 def get_face_embedding(x: np.ndarray) -> torch.Tensor:
@@ -12,3 +12,13 @@ def get_face_embedding(x: np.ndarray) -> torch.Tensor:
         return torch.tensor(faces[0].embedding)
     else:
         return torch.zeros(512)
+
+def get_face_embedding_tensor_batch(x: torch.Tensor) -> torch.Tensor:
+    x = x.permute(0, 2, 3, 1)
+    x = x.cpu().detach().numpy()
+    embeddings = []
+    for img in x:
+        embedding = get_face_embedding(img * 255)
+        embeddings.append(embedding)
+    embeddings = torch.stack(embeddings, dim=0)
+    return embeddings
--- train.py
+++ train.py
@@ -7,11 +7,16 @@ from dotenv import load_dotenv
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from random import random
+import numpy as np
+from PIL import Image
 
 from datasets.celeba import get_celeba_dataloader
 from model.identity_encoder import IdentityEncoder
 from model.message_encoder import MessageEncoder
 from model.watermark_encoder import WatermarkEncoder
+from model.utils import get_face_embedding_tensor_batch
 
 
 load_dotenv()
@@ -19,12 +24,6 @@ wandb.login(key=os.getenv("WANDB_API_KEY"))
 
 
 def get_train_dataloader() -> DataLoader:
-    """
-    Returns a DataLoader over a face dataset.
-
-    Uses a custom CelebA implementation that works with a manually
-    downloaded dataset placed under ``data/celeba``.
-    """
     return get_celeba_dataloader(
         root="./data/celeba",
         split="train",
@@ -34,6 +33,23 @@ def get_train_dataloader() -> DataLoader:
         shuffle=True,
         return_repr=True,
     )
+
+def save_training_image(original_image, watermarked_image, epoch, i):
+    original_image = original_image.permute(1, 2, 0).cpu().detach().numpy()
+    watermarked_image = watermarked_image.permute(1, 2, 0).cpu().detach().numpy()
+    original_image = (original_image * 255).astype(np.uint8)
+    watermarked_image = (watermarked_image * 255).astype(np.uint8)
+    original_image = Image.fromarray(original_image)
+    watermarked_image = Image.fromarray(watermarked_image)
+    # Combine the two images side by side and save
+    combined_image = Image.new(
+        "RGB",
+        (original_image.width + watermarked_image.width, max(original_image.height, watermarked_image.height)),
+    )
+    combined_image.paste(original_image, (0, 0))
+    combined_image.paste(watermarked_image, (original_image.width, 0))
+    os.makedirs("training_samples", exist_ok=True)
+    combined_image.save(f"training_samples/epoch{epoch}_iter{i}.png")
 
 
 def train() -> None:
@@ -70,11 +86,17 @@ def train() -> None:
     
     for epoch in range(num_epochs):
         running_loss = 0.0
+        running_cos_sim_loss = 0.0
+        running_watermark_lpips_loss = 0.0
+        running_watermarked_cos_sim_loss = 0.0
         num_batches = 0
 
         progress_bar = tqdm(dataloader, desc=f"Epoch {epoch + 1}/{num_epochs}", file=sys.stdout)
 
+        image_index = 0
         for images, id_vecs, _ in progress_bar:
+            images = images.to(device)
+            id_vecs = id_vecs.to(device)
             batch_size = images.size(0)
 
             # Random binary messages in {0, 1}
@@ -82,34 +104,62 @@ def train() -> None:
                 0, 2, (batch_size, message_dim), device=device, dtype=torch.float32
             )
 
-            # Use precomputed 512-d identity representations
-            id_vecs = id_vecs.to(device)  # (B, 512)
-
-            # Encode messages to 512-d vectors
             msg_vecs = message_encoder(messages)  # (B, 512)
-
             # Pass through IdentityEncoder to embed the message into identity representation
             out_vecs = identity_encoder(id_vecs, msg_vecs)  # (B, 512)
-
             # Cosine similarity loss between original and modified identity representations
             cos_sim = F.cosine_similarity(id_vecs, out_vecs, dim=1)
-            loss = 1 - cos_sim.mean()
+            cos_sim_loss = 1 - cos_sim.mean()
 
+            # Training watermark encoder by optimizing the LPIPS between original and watermarked images
+            watermarked_images = watermark_encoder(images, out_vecs)
+            watermarked_images = torch.clamp(watermarked_images, min=0.0, max=1.0)
+            lpips = LearnedPerceptualImagePatchSimilarity(net_type="squeeze", normalize=True).to(device)
+            watermark_lpips_loss = lpips(images, watermarked_images)
+
+            # and the cosini similarity loss between identity representations of original and watermarked images
+            watermarked_id_vecs = get_face_embedding_tensor_batch(watermarked_images)
+            watermarked_id_vecs = watermarked_id_vecs.to(device)
+            watermarked_cos_sim = F.cosine_similarity(id_vecs, watermarked_id_vecs, dim=1)
+            watermarked_cos_sim_loss = 1 - watermarked_cos_sim.mean()
+            
+            
+            total_loss = cos_sim_loss + watermark_lpips_loss + watermarked_cos_sim_loss
             optimizer.zero_grad()
-            loss.backward()
+            total_loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
+            running_loss += total_loss.item()
+            running_cos_sim_loss += cos_sim_loss.item()
+            running_watermark_lpips_loss += watermark_lpips_loss.item()
+            running_watermarked_cos_sim_loss += watermarked_cos_sim_loss.item()
             num_batches += 1
 
-            progress_bar.set_postfix({"loss": f"{loss.item():.4f}"})
+            progress_bar.set_postfix({"watermark_lpips_loss": f"{watermark_lpips_loss.item():.4f}", "cos_sim_loss": f"{cos_sim_loss.item():.4f}"})
+
+            if random() <= 0.01 or image_index <= 10:
+                save_training_image(images[0], watermarked_images[0], epoch, image_index)
+                image_index += 1
 
         avg_loss = running_loss / max(1, num_batches)
-        print(f"Epoch {epoch + 1}/{num_epochs} - Loss: {avg_loss:.4f}")
-        run.log({"loss": avg_loss})
+        avg_cos_sim_loss = running_cos_sim_loss / max(1, num_batches)
+        avg_watermark_lpips_loss = running_watermark_lpips_loss / max(1, num_batches)
+        avg_watermarked_cos_sim_loss = running_watermarked_cos_sim_loss / max(1, num_batches)
+        print(f"Epoch {epoch + 1}/{num_epochs} - Cosine similarity loss: {avg_cos_sim_loss:.4f}, Watermark LPIPS loss: {avg_watermark_lpips_loss:.4f}, Watermarked cosine similarity loss: {avg_watermarked_cos_sim_loss:.4f}, Total loss: {avg_loss:.4f}")
+        run.log({
+            "cos_sim_loss": avg_cos_sim_loss, 
+            "watermark_lpips_loss": avg_watermark_lpips_loss, 
+            "watermarked_cos_sim_loss": avg_watermarked_cos_sim_loss, 
+            "total_loss": avg_loss
+        })
 
+        os.makedirs("weights", exist_ok=True)
+        os.makedirs("weights/identity_encoder", exist_ok=True)
+        os.makedirs("weights/message_encoder", exist_ok=True)
+        os.makedirs("weights/watermark_encoder", exist_ok=True)
         torch.save(identity_encoder.state_dict(), f"weights/identity_encoder/identity_encoder_{epoch + 1}.pth")
         torch.save(message_encoder.state_dict(), f"weights/message_encoder/message_encoder_{epoch + 1}.pth")
+        torch.save(watermark_encoder.state_dict(), f"weights/watermark_encoder/watermark_encoder_{epoch + 1}.pth")
 
     run.finish()
 
